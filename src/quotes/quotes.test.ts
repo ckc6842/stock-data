2306105834444acde211ebd1a815be94dd8ff5d0
GC <ckc6842@gmail.com>
2021-07-20 22:05:33 +0900

    feat: 일봉 조회 API 요청 메소드 구현

--- src/quotes/quotes.test.ts
+++ src/quotes/quotes.test.ts
@@ -1,4 +1,15 @@
-import { findStocks } from './quotes'
+import { findStocks, makeRequests } from './quotes'
+import { SNP500Stock, SNP500StockDocument } from '../interfaces'
+import axios, { AxiosStatic } from 'axios'
+import { SNP500StockModel } from '../models'
+
+interface AxiosMock extends AxiosStatic {
+  mockResolvedValue: Function
+  mockRejectedValue: Function
+}
+
+jest.mock('axios')
+const axiosMock = axios as AxiosMock
 
 describe('일봉 API 요청 및 저장', () => {
   test.each([
@@ -9,5 +20,25 @@ describe('일봉 API 요청 및 저장', () => {
   ])('findStocks skip: %s, limit: %s에 대한 %s번째 index symbol은 %s이다.', async (skip: number, limit: number, index: number, symbol: string) => {
     const stocks = await findStocks(skip, limit)
     expect(stocks[index].symbol).toEqual(symbol)
+  })
+
+  test('요청 목록 생성 및 요청을 정상적으로 수행한다.', async () => {
+    const requests = await makeRequests([
+      new SNP500StockModel({symbol: 'MMM'}),
+      new SNP500StockModel({symbol: 'AAPL'}),
+    ])
+    await Promise.all(requests)
+    const start = new Date(Date.now() - 365 * 24 * 60 * 60 * 1000)
+      .toISOString()
+      .split('T')[0]
+    expect(axiosMock.get).toHaveBeenCalledWith(
+      `https://sandbox.tradier.com/v1/markets/history?symbol=MMM&start=${start}&interval=daily`,
+      expect.anything(),
+    )
+    expect(axiosMock.get).toHaveBeenCalledWith(
+      `https://sandbox.tradier.com/v1/markets/history?symbol=AAPL&start=${start}&interval=daily`,
+      expect.anything(),
+    )
+    expect(axiosMock.get).toHaveBeenCalledTimes(2)
   })
 })
--- src/quotes/quotes.ts
+++ src/quotes/quotes.ts
@@ -1,3 +1,4 @@
+import axios, { AxiosPromise } from 'axios'
 import { config } from 'dotenv'
 import { getConnection } from '../db'
 import { SNP500StockDocument } from '../interfaces'
@@ -8,4 +9,28 @@ export const findStocks = async (skip: number = 0, limit: number = 50): Promise<
   const db = await getConnection()
   const stocks = SNP500StockModel.find().sort({'_id': 1}).skip(skip).limit(limit).exec()
   return stocks
+}
+
+export const makeRequests = (stocks: Array<SNP500StockDocument>): Array<AxiosPromise> => {
+  const start = new Date(Date.now() - 365 * 24 * 60 * 60 * 1000)
+    .toISOString()
+    .split('T')[0]
+  return stocks.map((stock) => {
+    return requestQuotes(stock.symbol, start, 'daily')
+  })
+}
+
+const requestQuotes = (symbol: string, start: string, interval: string = 'daily'): AxiosPromise => {
+  const apiUrlBase = 'https://sandbox.tradier.com/v1'
+  const queries = {
+    symbol,
+    start,
+    interval,
+  }
+  const queryStringList = Object.entries(queries).map(([k, v]) => `${k}=${v}`)
+  return axios.get(`${apiUrlBase}/markets/history?${queryStringList.join('&')}`, {
+    headers: {
+      Authorization: `Bearer ${process.env.TRADIER_TOKEN}`
+    }
+  })
 }
