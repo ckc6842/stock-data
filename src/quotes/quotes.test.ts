b0d206a15e946f9b4418440776f1c8a85014e031
GC <ckc6842@gmail.com>
2021-07-26 09:26:41 +0900

    feat: 주가 API 변경

--- src/quotes/quotes.test.ts
+++ src/quotes/quotes.test.ts
@@ -1,7 +1,9 @@
-import { findStocks, makeRequests } from './quotes'
-import { SNP500Stock, SNP500StockDocument } from '../interfaces'
+import { makeRequests, requestQuotes } from './quotes'
 import axios, { AxiosStatic } from 'axios'
+import { config } from 'dotenv'
 import { SNP500StockModel } from '../models'
+
+config()
 
 interface AxiosMock extends AxiosStatic {
   mockResolvedValue: Function
@@ -13,21 +15,22 @@ const axiosMock = axios as AxiosMock
 
 describe('일봉 API 요청 및 저장', () => {
   test('요청 목록 생성 및 요청을 정상적으로 수행한다.', async () => {
-    const requests = await makeRequests([
-      new SNP500StockModel({symbol: 'MMM'}),
-      new SNP500StockModel({symbol: 'AAPL'}),
-    ])
-    await Promise.all(requests)
-    const start = new Date(Date.now() - 365 * 24 * 60 * 60 * 1000)
-      .toISOString()
-      .split('T')[0]
-    expect(axiosMock.get).toHaveBeenCalledWith(
-      `https://sandbox.tradier.com/v1/markets/history?symbol=MMM&start=${start}&interval=daily`,
-      expect.anything(),
+    const requests = await makeRequests(
+      [
+        new SNP500StockModel({symbol: 'MMM'}),
+        new SNP500StockModel({symbol: 'AAPL'}),
+      ],
+      requestQuotes
+    )
+    const response = await Promise.all(requests)
+
+    const apiUrlBase = 'https://www.alphavantage.co/query?'
+    const apiKey = process.env.ALPHA_VANTAGE_TOKEN
+    expect(axiosMock.get).toHaveBeenCalledWith (
+      `${apiUrlBase}function=TIME_SERIES_DAILY_ADJUSTED&symbol=MMM&apikey=${apiKey}`,
     )
     expect(axiosMock.get).toHaveBeenCalledWith(
-      `https://sandbox.tradier.com/v1/markets/history?symbol=AAPL&start=${start}&interval=daily`,
-      expect.anything(),
+      `${apiUrlBase}function=TIME_SERIES_DAILY_ADJUSTED&symbol=AAPL&apikey=${apiKey}`,
     )
     expect(axiosMock.get).toHaveBeenCalledTimes(2)
   })
--- src/quotes/quotes.ts
+++ src/quotes/quotes.ts
@@ -1,31 +1,31 @@
 import axios, { AxiosPromise } from 'axios'
 import { config } from 'dotenv'
-import { getConnection } from '../db/connection'
 import { SNP500StockDocument } from '../interfaces'
-import { SNP500StockModel } from '../models'
 config()
 
+const apiUrlBase = 'https://www.alphavantage.co/query?'
 
-export const makeRequests = (stocks: Array<SNP500StockDocument>): Array<AxiosPromise> => {
-  const start = new Date(Date.now() - 365 * 24 * 60 * 60 * 1000)
-    .toISOString()
-    .split('T')[0]
+const makeQueryUrl = (params: object): string => {
+  const queries = {
+    ...params,
+    apikey: process.env.ALPHA_VANTAGE_TOKEN,
+  }
+  const queryStringList = Object.entries(queries).map(([k, v]) => `${k}=${v}`)
+  return apiUrlBase + queryStringList.join('&')
+}
+
+export const makeRequests = (
+  stocks: Array<SNP500StockDocument>,
+  requestMethod: Function
+): Array<AxiosPromise> => {
   return stocks.map((stock) => {
-    return requestQuotes(stock.symbol, start, 'daily')
+    return requestMethod(stock.symbol)
   })
 }
 
-const requestQuotes = (symbol: string, start: string, interval: string = 'daily'): AxiosPromise => {
-  const apiUrlBase = 'https://sandbox.tradier.com/v1'
-  const queries = {
-    symbol,
-    start,
-    interval,
-  }
-  const queryStringList = Object.entries(queries).map(([k, v]) => `${k}=${v}`)
-  return axios.get(`${apiUrlBase}/markets/history?${queryStringList.join('&')}`, {
-    headers: {
-      Authorization: `Bearer ${process.env.TRADIER_TOKEN}`
-    }
-  })
+export const requestQuotes = (symbol: string): AxiosPromise => {
+  return axios.get(makeQueryUrl({
+    function: 'TIME_SERIES_DAILY_ADJUSTED',
+    symbol
+  }))
 }
